899330eb61232a34cc19624c790474d349dfa9d8
Morteza Hosseini <mortezahosseinee@gmail.com>
2019-02-12 11:50:07 +0330

    remove images from module

--- app/src/main/java/com/example/fasation/bottom/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/fasation/bottom/myapplication/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.example.fasation.bottom.myapplication
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
     }
 }

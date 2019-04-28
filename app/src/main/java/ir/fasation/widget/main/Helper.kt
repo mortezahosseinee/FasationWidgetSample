227df1e5582c52b6773cca8546d6f2ef25bdcc46
Morteza Hosseini <mortezahosseinee@gmail.com>
2019-04-28 17:34:50 +0430

    add fragments and split custom views

---
+++ app/src/main/java/ir/fasation/widget/main/Helper.kt
@@ -0,0 +1,24 @@
+package ir.fasation.widget.main
+
+import androidx.annotation.AnimRes
+import androidx.annotation.AnimatorRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+fun replaceFragment(activity: AppCompatActivity, fragment: Fragment?, @AnimatorRes @AnimRes enter: Int, @AnimatorRes @AnimRes exit: Int) {
+    if (fragment == null) return
+
+    activity.supportFragmentManager.inTransaction {
+        addToBackStack(fragment::class.simpleName)
+        setCustomAnimations(enter, exit)
+        replace(R.id.frm_main, fragment)
+    }
+}
+
+inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+    val fragmentTransaction = beginTransaction()
+    fragmentTransaction.func()
+    fragmentTransaction.commit()
+}
--- app/src/main/java/ir/fasation/widget/main/MainActivity.kt
+++ app/src/main/java/ir/fasation/widget/main/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.addOnBackStackChangedListener { if (supportFragmentManager.backStackEntryCount == 0) finish() }
+        replaceFragment(this, MainFragment.newInstance(), R.anim.fade_in, R.anim.fade_out)
     }
 }
